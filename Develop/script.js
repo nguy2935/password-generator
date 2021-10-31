fc4a362c4bed223d488456fb1a6cd2ca4bbee7a8
unknown <nguy2935@umn.edu>
2021-10-31 14:04:42 -0500

    adding some randomizers for passwords

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,31 @@
 // Assignment code here
+document.getElementById('uppercase');
+document.getElementById('lowercase');
+document.getElementById('numbers');
+document.getElementById('symbols');
+document.getElementById('generate');
+
+// will generate an uppercase letter //
+function getRandomUpper() {
+  console.log(Math.floor(Math.random() * 26) + 65);
+}
+
+console.log(getRandomUpper());
+
+// will generate a number //
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+console.log(getRandomNumber());
+
+// this will generate a random symbol //
+function getRandomSymbol() {
+  var symbols = "!@#$%^&*(){}<>=/.,`";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomSymbol());
 
 
 // Get references to the #generate element
@@ -14,4 +41,8 @@ function writePassword() {
 }
 
 // Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
+writePassword = window.prompt("Password Criteria: Select one. Type 1 for at least 8 characters and no more than 128 characters, Type 2 for at least 123 characters and no more than 1000 characters, Type 3 for at least 456 characters and no more than 2000 chracters");
+writePassword = parseInt(writePassword)
+// element.addEventListener("click", function(){ alert("Password Criteria:</br> at least 8 characters and no more than 128 characters"); });
