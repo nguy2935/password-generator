86b7a2950ab5889e34a49e76441ca6d4760eac36
unknown <Sophia Nguyen>
2021-11-23 18:16:07 -0600

    updated README with criterias

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,13 @@
 // Assignment code here
+// declare user input variables that will be used
+var characters = '`~!@#$%^&*()_+-=[]{}\:;,.<>/';
+var length;
+var numbs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var upperCase;
+var lowerCase;
+var strings = 'abcdefghijklmnopqrstuvwxyz';
+
+// need a function here to generate a random password
 
 
 // Get references to the #generate element
